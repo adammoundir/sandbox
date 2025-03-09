d48e2f3e7d4a930df990fc81a1de5bce8e9aa409
adammoundir <adam.moundir@hotmail.com>
2025-03-09 15:11:59 +0100

    First update

--- test2.R
+++ test2.R
@@ -1,3 +1,33 @@
 test2
 
 Test3
+library(tidyverse)
+library(rvest)
+library(httr)
+install.packages('usethis')
+usethis::create_github_token()
+
+browseURL('https://revendo.com/de-ch/robots.txt')
+browseURL('https://www.jusit.ch/robots.txt')
+download.file('https://revendo.com/de-ch/')
+download.file(url = 'https://revendo.com/de-ch/', destfile = 'test.html')
+
+url <- 'https://revendo.com/de-ch/kaufen/k/iphone/'
+
+read_html(url) %>% 
+  html_elements(css = '.price , .product-name') %>% 
+  html_text()
+
+urluni <- 'https://www.unilu.ch/'
+
+page <- read_html(urluni)
+
+page2 <- httr::GET(url = 'https://www.unilu.ch/')
+page3 <- content(page2)
+install.packages('jsonlite')
+library(jsonlite)
+
+
+revendo <- GET(url = 'www.revendo.ch')
+
+cnt <- content(revendo)
